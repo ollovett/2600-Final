e5092507800915995f9a0737741e8be8cb888aba
Owen Lovett <olovett214@gmail.com>
2022-12-15 22:43:43 -0800

    add mqtt

--- board/board.ino
+++ board/board.ino
@@ -3,6 +3,22 @@
 #include <IRremoteESP8266.h>
 #include <IRrecv.h>
 #include <IRutils.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// WiFi
+const char *ssid = ""; // keeping private
+const char *password = "";  // keeping private
+
+// MQTT Broker
+const char *mqtt_broker = "broker.emqx.io";
+const char *topic = "esp32/test";
+const char *mqtt_username = "emqx";
+const char *mqtt_password = "public";
+const int mqtt_port = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient)
 
 const uint16_t recvPin = 13; // Infrared receiving pin
 IRrecv irrecv(recvPin);      // Create a class object used to receive class
@@ -42,6 +58,46 @@ void setup() {
   ledcAttachPin(ledPin8,7);
   ledcAttachPin(ledPin9,8);
   irrecv.enableIRIn();        // Start the receiver
+
+  // Set software serial baud to 115200;
+  Serial.begin(115200);
+  // connecting to a WiFi network
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+  //connecting to a mqtt broker
+  client.setServer(mqtt_broker, mqtt_port);
+  client.setCallback(callback);
+  while (!client.connected()) {
+      String client_id = "esp32-client-";
+      client_id += String(WiFi.macAddress());
+      Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
+      if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+          Serial.println("Public emqx mqtt broker connected");
+      } else {
+          Serial.print("failed with state ");
+          Serial.print(client.state());
+          delay(2000);
+      }
+  }
+  // publish and subscribe
+  client.publish(topic, "Hi EMQX I'm ESP32 ^^");
+  client.subscribe(topic);
+  }
+}
+
+void callback(char *topic, byte *payload, unsigned int length) {
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+      Serial.print((char) payload[i]);
+  }
+  Serial.println();
+  Serial.println("-----------------------");
 }
 
 void loop() {
@@ -49,6 +105,7 @@ void loop() {
     handleControl(results.value);       // Handle the commands from remote control
     irrecv.resume();                    // Receive the next value
   }
+  client.loop();
 }
 
 void handleControl(unsigned long value) {
